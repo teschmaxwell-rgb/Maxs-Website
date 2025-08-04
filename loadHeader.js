cf2f826ffaa0e854d9be469d00bd28993310f79a
Max Tesch <maxwell.tesch@gmail.com>
2025-08-04 12:33:25 -0500

    added error handling for header javascript

--- loadHeader.js
+++ loadHeader.js
@@ -1,7 +1,12 @@
+console.log("Loading header script...");
 document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM loaded. Fetching header....");
+
     fetch("/Maxs-Website/header.html")
     .then(response => response.text())
     .then(data => {
+        console.log("Header fetched:", data);
         document.getElementById("header").innerHTML = data;
     })
-})
+    .catch(err => console.error("Header fetch failed:",err ));
+});
